57fbe26bc318ca55f3bd0a99d6f869fd21656a59
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2022-11-25 21:14:33 +0300

    added prog-1

---
+++ examp_1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Напишите программу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второго.
+ int number1 = int.Parse(Console.ReadLine());
+ int number2 = int.Parse(Console.ReadLine());
+
+ int result = number1 * number1;
+ if (number2 == result) 
+ {
+Console.WriteLine("первое число является квадратом второго");
+ }
+ else 
+ {
+    Console.WriteLine("первое число не является квадратом второго");
+ }
---
+++ examp_2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Случайные числа
+
+Random random = new Random();
+int number = random.Next(10,100);
+
+int firstNumber = number / 10;
+int lastNumber = number % 10;
+
+if(firstNumber > lastNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.WriteLine(lastNumber);
+}
