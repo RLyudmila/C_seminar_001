987920a1819d8b06fde98720faa49c78497908ed
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2022-11-26 10:47:14 +0300

    an even number

---
+++ examp_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("YES");
+}else
+{
+  Console.WriteLine("NO");  
+}
