cc408fe5c4bc18434c4baf06a9150c5b1c4cdf0f
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2022-11-26 10:01:40 +0300

    Three numbers

--- examp_1/Program.cs
+++ examp_1/Program.cs
@@ -1,20 +1,17 @@
-﻿
-// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
-//а какое меньшее
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
  
  int number1 = int.Parse(Console.ReadLine());
  int number2 = int.Parse(Console.ReadLine());
- 
+
  Console.WriteLine(number1);
  Console.WriteLine(number2);
 
  if (number1 > number2) 
  {
-int max = number1;
+   Console.WriteLine(number1);
  }
  else 
  {
-   int max = number2;
-    }
- Console.Write("max=", max);
+   Console.WriteLine(number2);
+ } 
  
---
+++ examp_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+
+if ((number1 > number2) | (number1 > number3))
+{
+ Console.WriteLine(number1);
+}
+else
+if (number2 > number3)
+{
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.WriteLine(number3);
+}
