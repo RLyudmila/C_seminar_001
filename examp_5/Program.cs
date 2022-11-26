7ce1171ba8bb44f10d8b166c4a335d2ecacc8ebc
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2022-11-26 11:14:09 +0300

    all even numbers from 1 to N.

---
+++ examp_5/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int N = int.Parse(Console.ReadLine());
+int k;
+k = 2;
+while (k<=N)
+{
+    Console.WriteLine(k);
+    k = k +2;
+}
+
+
